c6b84c8cc1ef7a2ad551ad206b1c6763136523f0
Vanius <vanius@hotmail.com>
2017-12-18 18:18:52 -0200

    Criando bd h2 e criando classe e tabela via jpa

--- src/main/java/com/vanius/cursomc/domain/Categoria.java
+++ src/main/java/com/vanius/cursomc/domain/Categoria.java
@@ -2,12 +2,22 @@ package com.vanius.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
