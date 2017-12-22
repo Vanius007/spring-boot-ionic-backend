ee126536fee5aa3b861933eb8aa5760f4dd7aee8
Vanius <vanius@hotmail.com>
2017-12-22 16:20:53 -0200

    validação personalizada de cpf e cnpj na inserção de cliente

--- src/main/java/com/vanius/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/vanius/cursomc/dto/ClienteNewDTO.java
@@ -2,25 +2,45 @@ package com.vanius.cursomc.dto;
 
 import java.io.Serializable;
 
-import com.vanius.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import com.vanius.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres.")	
 	private String nome;
-	private String email;
-	private String cpfOuCnpj;
-	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="e-mail inválido")
+	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	private String cpfOuCnpj;
+	
+	private Integer tipo;
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
