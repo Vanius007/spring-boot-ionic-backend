e23a6dc340a794caa8038cb705db0d760bc0e4f8
Vanius <vanius@hotmail.com>
2017-12-27 21:15:08 -0200

    Configuração Inicial do Spring Security

--- src/main/java/com/vanius/cursomc/domain/Pedido.java
+++ src/main/java/com/vanius/cursomc/domain/Pedido.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.vanius.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class Pedido implements Serializable {
