af0f22caf1c09e1f49cdf6c1a09e4ded6befcd0f
Vanius <vanius@hotmail.com>
2018-01-06 12:05:57 -0200

    Enviando Imagem via endpoint

--- src/main/java/com/vanius/cursomc/resources/ClienteResource.java
+++ src/main/java/com/vanius/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vanius.cursomc.domain.Cliente;
@@ -62,6 +63,7 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);	
 		
 	}
+	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
 		Cliente obj = service.fromDTO(objDto);
@@ -88,6 +90,14 @@ public class ClienteResource {
 		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);
+	
+		return ResponseEntity.created(uri).build();
 		
 	}
 
--- src/main/java/com/vanius/cursomc/services/S3Service.java
+++ src/main/java/com/vanius/cursomc/services/S3Service.java
@@ -1,16 +1,24 @@
 package com.vanius.cursomc.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.management.RuntimeErrorException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
@@ -24,23 +32,36 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 	
-	public void uploadFile(String localPath) {
+	public URI uploadFile(MultipartFile multiPartFile ) {
 		
-		try {
+			try {
+				String fileName = multiPartFile.getOriginalFilename();
+				InputStream is = multiPartFile.getInputStream();
+				String contentType = multiPartFile.getContentType();
+				return uploadFile(is, fileName, contentType );
+			} catch (IOException e) {
+				throw new RuntimeException("Erro de IO: " + e.getMessage());
+			}
 			
-			File file = new File(localPath);
-			LOG.info("Iniciando envio de arquivo...");
-			s3client.putObject(new PutObjectRequest(bucketName, "teste", file));
-			LOG.info("Envio de arquivo finalizado");
 			
-		} catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " +e.getMessage());
-			LOG.info("Status code: " + e.getErrorCode());
-		} catch(AmazonClientException e){
-			LOG.info("AmazonClientException: " +e.getMessage());
 			
-		}
 		
+		
+		
+	}
+	public URI uploadFile(InputStream is,  String fileName, String contentType ) {
+			
+			try {
+				ObjectMetadata meta = new ObjectMetadata();
+				meta.setContentType(contentType);
+				LOG.info("Iniciando envio de arquivo...");
+				s3client.putObject(bucketName, fileName, is, meta );
+				LOG.info("Envio de arquivo finalizado");
+				return s3client.getUrl(bucketName, fileName).toURI();
+			} catch (URISyntaxException e) {
+				throw new RuntimeException("Erro ao converter URL par URI");
+			}
+
 		
 	}
 }
