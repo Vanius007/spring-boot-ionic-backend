511efc65c6b402f64a36890b4528bd148e2814cc
Vanius <vanius@hotmail.com>
2018-01-06 11:23:04 -0200

    Salvando o primeiro arquino no amazon

---
+++ src/main/java/com/vanius/cursomc/services/S3Service.java
@@ -0,0 +1,46 @@
+package com.vanius.cursomc.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+  
+	private Logger LOG =  LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadFile(String localPath) {
+		
+		try {
+			
+			File file = new File(localPath);
+			LOG.info("Iniciando envio de arquivo...");
+			s3client.putObject(new PutObjectRequest(bucketName, "teste", file));
+			LOG.info("Envio de arquivo finalizado");
+			
+		} catch (AmazonServiceException e) {
+			LOG.info("AmazonServiceException: " +e.getMessage());
+			LOG.info("Status code: " + e.getErrorCode());
+		} catch(AmazonClientException e){
+			LOG.info("AmazonClientException: " +e.getMessage());
+			
+		}
+		
+		
+	}
+}
